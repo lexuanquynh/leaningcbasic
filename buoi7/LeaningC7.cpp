fa2b8bfba123428efc2a694eac1294b6158d01b5
quynhlx <quynhlxbkhn@gmail.com>
2020-11-09 23:32:26 +0900

    source buoi 7

---
+++ buoi7/LeaningC7.cpp
@@ -0,0 +1,27 @@
+// Program to take 5 values from the user and store them in an array 
+// Print the elements stored in the array 
+/*#include <stdio.h> 
+
+int main() 
+{
+	int values[5];
+
+	printf_s("Enter 5 integers: ");
+
+	// taking input and storing it in an array 
+	for (int i = 0; i < 5; ++i) 
+	{
+		scanf_s("%d", &values[i]);
+	}
+
+	printf_s("Displaying integers: ");
+
+	// printing elements of an array 
+	for (int i = 0; i < 5; ++i) 
+	{ 
+		printf("%d\n", values[i]); 
+	}
+
+	return 0;
+}
+*/
---
+++ buoi7/Source.cpp
@@ -0,0 +1,34 @@
+// Program to find the average of n numbers using arrays 
+/*#include <stdio.h> 
+
+int main() 
+{
+	int marks[10], i, n, sum = 0;
+	float average;
+
+	do 
+	{
+		printf("Enter number of elements: ");
+		scanf_s("%d", &n);
+
+		if (n > 10)
+		{
+			printf("May nhap gia tri loon hon 10 roi  nhap lai di\n");
+		}
+	} while (n > 10);
+
+	for (i = 0; i < n; ++i) 
+	{
+		printf("Enter number %d: ", i + 1); 
+		scanf_s("%d", &marks[i]);
+		// adding integers entered by the user to the sum variable 
+		sum += marks[i]; 
+	}
+
+	average = (float)sum / (float)n;
+
+	printf("Average = %f", average);
+
+	return 0;
+}
+*/
